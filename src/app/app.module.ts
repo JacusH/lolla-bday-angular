d6b3aaa3f11b0d57972ecc990fb3fa88126e536f
Jacus Horne <jacus.horne@sprinthive.com>
2023-02-23 18:40:50 +0200

    Added menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,31 @@ import { ProductsComponent } from './products/products.component';
 import { ProductHousesComponent } from './product-houses/product-houses.component';
 
 import { HttpClientModule } from "@angular/common/http";
+import { AdminComponent } from './admin/admin.component';
+import { ProductHouseFormComponent } from './admin/product-house-form/product-house-form.component';
+import { MenuComponent } from './menu/menu.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductHousesComponent
+    ProductHousesComponent,
+    AdminComponent,
+    ProductHouseFormComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.sass']
+})
+export class MenuComponent {
+
+  loggedIn = false
+  admin = true
+
+}
